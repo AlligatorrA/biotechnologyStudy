dd40550273e8bc6f46c834423a4c4ebbdd6a69b4
Anand <anandraj143kick@gmail.com>
2021-11-29 15:36:16 +0530

    Modified css

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-const cart = document.querySelector(".cart")
+const menu = document.querySelector(".menu")
 const popUp = document.querySelector(".popUp")
+const linkResponsive = document.querySelector(".link-responsive")
 
-cart.addEventListener("click", cartFunction)
+menu.addEventListener("click", menuFunction)
 
-function cartFunction(e) {
-    e.preventDefault()
-    popUp.innerHTML = `Added to cart`
-    setTimeout(() => {
-        popUp.innerHTML = ``
-    }, 3000);
+// function cartFunction(e) {
+//     e.preventDefault()
+//     popUp.innerHTML = `Added to cart`
+//     setTimeout(() => {
+//         popUp.innerHTML = ``
+//     }, 3000);
+// }
+
+function menuFunction() {
+    console.log("hello");
+    if (menu.checked == true) {
+        linkResponsive.style.display = "block";
+    }else{
+        linkResponsive.style.display = "none";
+    }
+    
 }
